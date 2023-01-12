5c160beadbdfb46e47ef92cde2c9a664426162aa
MYD <2663797538@qq.com>
2023-01-12 17:53:11 +0800

    change font and md theme

--- src/components/MarkdownLoader/MarkdownLoader.tsx
+++ src/components/MarkdownLoader/MarkdownLoader.tsx
@@ -2,9 +2,10 @@ import 'github-markdown-css/github-markdown-light.css'
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
-import {materialLight} from "react-syntax-highlighter/dist/esm/styles/prism";
+import {oneLight, oneDark} from "react-syntax-highlighter/dist/esm/styles/prism";
 import React, {FC} from "react";
 
+const highlighterStyle: any = window.matchMedia('(prefers-color-scheme: dark)').matches ? oneDark : oneLight;
 const MarkdownLoader: FC<{ markdown: string }> = ({markdown}) => {
   return (
     <div className="markdown-body" style={{background: "transparent"}}>
@@ -18,8 +19,7 @@ const MarkdownLoader: FC<{ markdown: string }> = ({markdown}) => {
               return !inline && match ? (
                 <SyntaxHighlighter
                   children={String(children).replace(/\n$/, '')}
-                  // @ts-ignore
-                  style={materialLight}
+                  style={highlighterStyle}
                   language={match[1]}
                   PreTag="div"
                   {...props}
