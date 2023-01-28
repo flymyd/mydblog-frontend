a3e7d351617407b58b1c28735392634c4d731c66
MYD <2663797538@qq.com>
2023-01-28 11:37:38 +0800

    test gif

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,9 +6,18 @@ const Home: FC = () => {
   useEffect(()=>{
     const imgUrl = new URL('./assets/stinger.gif', import.meta.url).href
     console.log("%c", `padding:50px 300px;line-height:120px;background:url(${imgUrl}) no-repeat;`);
+    console.log("%c", `padding:50px 300px;line-height:120px;background:url('/stinger.gif') no-repeat;`);
     console.log(
       "%c ",
       `background-image: url(${imgUrl});
+      background-size: contain;
+      background-repeat: no-repeat;
+      padding: 200px;
+      `
+    )
+    console.log(
+      "%c ",
+      `background-image: url('/stinger.gif');
       background-size: contain;
       background-repeat: no-repeat;
       padding: 200px;
