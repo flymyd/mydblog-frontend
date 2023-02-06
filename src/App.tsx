3f169d5d17428ffa2bff43573e3817eeb61222d8
MYD <2663797538@qq.com>
2023-02-06 16:57:16 +0800

    add unique visitor counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,15 @@ import './App.css'
 import router from './router/index';
 import '@/assets/css/Framework/HideScrollBar.css'
 import {useEffect} from "react";
-import stingerTrigger from "@/utils/stinger";
 
 function App() {
   let element = useRoutes(router);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [element])
-  useEffect(() => {
-    stingerTrigger()
-  }, [])
+  // useEffect(() => {
+  //   stingerTrigger()
+  // }, [])
   return <div className="App">{element}</div>;
 }
 
--- src/Framework/Footer.tsx
+++ src/Framework/Footer.tsx
@@ -1,15 +1,26 @@
 import FooterStyle from "@/assets/css/Framework/Footer.module.scss";
 import useWindowWidth from "@/hooks/useWindowWidth";
+import {useEffect, useState} from "react";
+import {get} from "@/utils/request";
 
 function Footer() {
   const width = useWindowWidth();
+  const [visitors, setVisitors] = useState(0)
+  useEffect(() => {
+    get('/caches/getLastDayVisitors').then((res: any) => {
+      if (res.statusCode === 200) {
+        setVisitors(res.data)
+      }
+    })
+  }, [])
   return (
     <div
-      className={['flex items-center justify-center', width < 768 ? 'flex-col' : 'flex-row', FooterStyle['blog-footer']].join(" ")}
+      className={['flex items-center justify-center', 'flex-col', FooterStyle['blog-footer']].join(" ")}
       style={{fontSize: width < 768 ? 14 : 16, fontWeight: 200}}
     >
       <span>Copyleft © 2022 Flymyd. All rights not reserved.</span>
-      {/*<span style={width < 768 ? {marginTop: 10} : {marginLeft: 20}}>待备案</span>*/}
+      <span style={width < 768 ? {marginTop: 10} : {marginLeft: 20}}>Deployed on Vercel, Accelerated & Secured by Cloudflare.</span>
+      <span style={width < 768 ? {marginTop: 10} : {marginLeft: 20}}>今日独立访客数：{visitors}</span>
     </div>
   )
 }
