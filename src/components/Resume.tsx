7b21b7d288ef8cded34c48dd138e42d02b6c5d45
MYD <2663797538@qq.com>
2023-02-06 19:17:21 +0800

    test fix autoresize

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,17 +1,28 @@
-import {FC, useState} from "react";
+import {FC, useEffect, useState} from "react";
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import pdfWorker from 'pdfjs-dist/build/pdf.worker.js?url';
 import {Document, Page} from 'react-pdf/dist/esm/entry.vite';
 import {pdfjs} from "react-pdf";
-import useWindowWidth from "@/hooks/useWindowWidth";
 
 pdfjs.GlobalWorkerOptions.workerSrc = pdfWorker;
 const resumeUrl = 'https://mydblog.obs.cn-east-3.myhuaweicloud.com/resume.pdf'
 const Resume: FC = () => {
   const [numPages, setNumPages] = useState(null);
-  const windowWidth = useWindowWidth();
-
+  const [width, setWidth] = useState(0);
+  const resizeUpdate = (e: any) => {
+    // 通过事件对象获取浏览器窗口的高度
+    let w = e.target.innerWidth;
+    setWidth(w);
+  };
+  useEffect(() => {
+    let w = window.innerWidth;
+    setWidth(w)
+    window.addEventListener('resize', resizeUpdate);
+    return () => {
+      window.removeEventListener('resize', resizeUpdate);
+    }
+  }, []);
   function onDocumentLoadSuccess({numPages}: { numPages: any }) {
     setNumPages(numPages);
   }
@@ -19,7 +30,7 @@ const Resume: FC = () => {
   return (
     <Document file={resumeUrl} onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error}>
       {Array.from(new Array(numPages), (el, index) => <Page key={`page_${index + 1}`} pageNumber={index + 1}
-                                                            width={windowWidth < 980 ? windowWidth * 0.9 : 980}/>)}
+                                                            width={width < 736 ? width * 0.9 : 736}/>)}
     </Document>
   )
 }
