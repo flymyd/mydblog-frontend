b3a25b112926f32f9594441d29a8805981152f65
MYD <2663797538@qq.com>
2023-02-06 11:43:52 +0800

    add escape

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import {HashRouter} from 'react-router-dom'
 import '@/assets/css/Framework/HideScrollBar.css'
+import {MantineProvider} from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <HashRouter>
-    <App/>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App/>
+    </MantineProvider>
   </HashRouter>
-)
+);
